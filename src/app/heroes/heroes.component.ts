a4f2c9d86a10d667c634d9449a1537a393457fb1
Younnes Chebli <younnesc@gmail.com>
2023-09-28 01:33:06 +0200

    feat(navigation): update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,8 +13,6 @@ import { MessageService } from '../message.service';
 export class HeroesComponent {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
@@ -24,10 +22,5 @@ export class HeroesComponent {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
