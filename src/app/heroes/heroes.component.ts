aa8618ae37c7fa823fce78ee399cc94094eb595f
Younnes Chebli <younnesc@gmail.com>
2023-09-29 12:55:38 +0200

    feat(delete hero): added delete hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -32,4 +32,9 @@ export class HeroesComponent {
         this.heroes.push(hero);
       });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
